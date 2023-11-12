d0c3c27fe832f84edba7ac6c8f9d2c45fb0caf2d
Yace Sf <yacine.sf@outlook.fr>
2023-11-12 21:12:26 +0100

    Form lier a netlify

--- src/page/secton-contact/index.jsx
+++ src/page/secton-contact/index.jsx
@@ -3,16 +3,19 @@ import "./index.css";
 function Contact() {
   return (
     <section className="sections" id="contacts">
-      <h2 className="section-title" id="title-section">CONTACT</h2>
+      <h2 className="section-title" id="title-section">
+        CONTACT
+      </h2>
       <div className="title-line"></div>
-      <form id="contactForm" name="contact" data-netlify="true">
-          <label htmlFor="name">Nom :</label>
-          <input type="text" id="name" name="name" required />
-          <label htmlFor="email">E-mail :</label>
-          <input type="text" id="email" name="email" required />
-          <label htmlFor="message">Message :</label>
-          <textarea id="message" name="message" rows="4"></textarea>
-      <button type="submit">Envoyer</button>
+      <form id="contactForm" method="POST" name="contact" data-netlify="true" onSubmit="submit">
+        <input type="hidden" name="form-name" value="contact" />
+        <label htmlFor="name">Nom :</label>
+        <input type="text" id="name" name="name" required />
+        <label htmlFor="email">E-mail :</label>
+        <input type="email" id="email" name="email" required />
+        <label htmlFor="message">Message :</label>
+        <textarea id="message" name="message" rows="4"></textarea>
+        <button type="submit">Envoyer</button>
       </form>
     </section>
   );
