2ad9353926779cbcf6f7cf4b32c65f981b6da382
Yace Sf <yacine.sf@outlook.fr>
2023-11-12 22:33:52 +0100

    Ajout d'un mailto et du github dans le footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,12 @@
 import "./index.css";
 
 function Footer() {
+const emailAddress = "yacine.sf@outlook.fr"
   return (
     <footer id="footer">
       <div className="div-icon">
-        <i className="fa-brands fa-github icon"></i>
-        <i className="fa-solid fa-envelope icon"></i>
+        <a href="https://github.com/Yacesf"><i className="fa-brands fa-github icon"></i></a>
+        <a href={`mailto:${emailAddress}`}><i className="fa-solid fa-envelope icon"></i></a>
       </div>
       <p>© 2023 Yacine SAFSAFI, Tous droits réservés.</p>
     </footer>
