db7525b5051119616357b0d0c8a1baa499a5c699
Yace Sf <yacine.sf@outlook.fr>
2023-11-27 22:49:53 +0100

    modification de text sur les cartes de projet et ajout d'animation sur les sections

--- src/page/page.jsx
+++ src/page/page.jsx
@@ -3,8 +3,66 @@ import About from "./section-about";
 import Skill from './section-competences';
 import Projet from './section-projet';
 import Contact from './secton-contact';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
+import { ScrollToPlugin, ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
 function Page() {
+  const slideSectionright = (elem) => {
+    gsap.fromTo(
+      elem, {
+        opacity: 0,
+        x: -300
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 0.6,
+        scrollTrigger: {
+          trigger: elem,
+          start: "top center",
+          end: "bottom center",
+        }
+      }
+    )
+  }
+  const slideSectionleft = (elem) => {
+    gsap.fromTo(
+      elem, {
+        opacity: 0,
+        x: 300
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 0.6,
+        scrollTrigger: {
+          trigger: elem,
+          start: "top center",
+          end: "bottom center",
+        }
+      }
+    )
+  }
+
+  useEffect(() => {
+    slideSectionright("#about")
+  }, [])
+
+  useEffect(() => {
+    slideSectionleft("#skill")
+  }, [])
+
+  useEffect(() => {
+    slideSectionright("#projet")
+  }, [])
+
+  useEffect(() => {
+    slideSectionleft("#contacts")
+  }, [])
+
   return (
     <main>
     <About/>
